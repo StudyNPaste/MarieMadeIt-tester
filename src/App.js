417dd0b367e8b0b0383a8f7b29a509a613ac8c43
StudyNPaste <139663796+StudyNPaste@users.noreply.github.com>
2024-07-13 08:50:03 -0700

    html and css updates to the form

--- src/App.js
+++ src/App.js
@@ -8,6 +8,31 @@ function App() {
         <h1>MarieMadeIt</h1>
         <img src={logo} alt="" width={120} height={120} />
       </div>
+      <div className='title'>
+        <h1>Tester Application</h1>
+        <h3>Crochet Bloomers</h3>
+      </div>
+      <div>
+        <form className='info' action="">
+          <div className='info-input'>
+            <label htmlFor="">Instagram:</label>
+            <input type="text" placeholder='Instagram Name...' className='input' id='insta-name' autoComplete='off'/>
+          </div>
+          <div className='info-input'>
+            <label htmlFor="">Email:</label>
+            <input type="text" placeholder='Email Address...' className='input' id='email' autoComplete='off'/>
+          </div>
+          <div className='info-input'>
+            <label htmlFor="">Your Instagram URL:</label>
+            <input type="text" placeholder='Instagram URL...' className='input' id='insta-url' autoComplete='off'/>
+          </div>
+          <div>
+            <label htmlFor="">Color:</label>
+            <input type="text" placeholder='What color do you plan on using?...' className='input' id='item-color' autoComplete='off'/>
+          </div>
+          <button>SUBMIT</button>
+        </form>
+      </div>
     </div>
   );
 }
