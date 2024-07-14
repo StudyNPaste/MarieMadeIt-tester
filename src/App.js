396bbe1ebf16e68be2f2c8289c090ab904001498
StudyNPaste <139663796+StudyNPaste@users.noreply.github.com>
2024-07-13 17:59:09 -0700

    emails 200ok

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import './App.css';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import logo from './Components/Assets/logo.PNG'
 
 function App() {
+  const form = useRef();
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    const serviceID = 'service_8y90p28';
+    const templateID = 'template_4hi1yp5'
+
+    emailjs.sendForm(serviceID, templateID, form.current, {
+        publicKey: 'Y39E39t8rUfDdglmb',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+      e.target.reset()
+  };
+
   return (
     <div className="App">
       <div className='navbar'>
@@ -13,28 +37,28 @@ function App() {
         <h3>Crochet Bloomers</h3>
       </div>
       <div>
-        <form className='info' action="">
+        <form ref={form} onSubmit={sendEmail} className='info' action="">
           <div className='info-input'>
             <label htmlFor="">Instagram:</label>
-            <input type="text" name='ig' placeholder='Instagram Name...' className='input' id='insta-name' autoComplete='off'/>
+            <input type="text" name='ig' placeholder='Instagram Name...' className='input' id='insta-name' autoComplete='off' required/>
           </div>
           <div className='error-txt'>Instagram name can't be blank.</div>
           <div className='info-input'>
             <label htmlFor="">Your Instagram URL:</label>
-            <input type="text" name='url' placeholder='Instagram URL...' className='input' id='insta-url' autoComplete='off'/>
+            <input type="text" name='url' placeholder='Instagram URL...' className='input' id='insta-url' autoComplete='off'required/>
           </div>
           <div className='error-txt'>Instagram URL can't be blank.</div>
           <div className='info-input'>
             <label htmlFor="">Email:</label>
-            <input type="text" name='email' placeholder='Email Address...' className='input' id='email' autoComplete='off'/>
+            <input type="text" name='email' placeholder='Email Address...' className='input' id='email' autoComplete='off' required/>
           </div>
           <div className='error-txt'>Email Address can't be blank.</div>
           <div className='info-input'>
             <label htmlFor="">Color:</label>
-            <input type="text" name='color' placeholder='What color do you plan on using?...' className='input' id='item-color' autoComplete='off'/>
+            <input type="text" name='color' placeholder='What color do you plan on using?...' className='input' id='item-color' autoComplete='off' required/>
           </div>
           <div className='error-txt'>Please enter a color.</div>
-          <button>SUBMIT</button>
+          <input type='submit' className='submit-btn'></input>
         </form>
       </div>
     </div>
